b2c15aeeebeaab18c9b332c7d0e4321ff08c283b
Gisele <gisesonia@gmail.com>
2026-08-25 11:51:33 -0300

    Criando CepEntity para o banco de dados Room

--- app/src/main/java/com/example/buscacep/data/local/CepEntity.kt
+++ app/src/main/java/com/example/buscacep/data/local/CepEntity.kt
@@ -2,17 +2,6 @@ package com.example.buscacep.data.local
 
 import androidx.room3.Entity
 import androidx.room3.PrimaryKey
-
-@Entity(tableName = "cep_table")
-data class CepEntity(
-    @PrimaryKey val id: Int = SINGLE_ROW_ID,
-    val cep: String
-) {
-    companion object {
-        const val SINGLE_ROW_ID = 1
-    }
-}
-
 @Entity(tableName = "cep_table")
 data class CepEntity(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
