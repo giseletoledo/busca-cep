809fef4a7b2cb600bdf1a99262dd55443e62a829
Gisele <gisesonia@gmail.com>
2026-08-25 11:58:49 -0300

    Criando CepEntity para o banco de dados Room

--- app/src/main/java/com/example/buscacep/data/local/CepDao.kt
+++ app/src/main/java/com/example/buscacep/data/local/CepDao.kt
@@ -2,14 +2,14 @@ package com.example.buscacep.data.local
 
 import androidx.room3.Dao
 import androidx.room3.Insert
-import androidx.room3.OnConflictStrategy
 import androidx.room3.Query
 
 @Dao
 interface CepDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun saveCep(cepEntity: CepEntity)
 
-    @Query("SELECT * FROM cep_table WHERE id = :id")
-    suspend fun getCep(id: Int = CepEntity.SINGLE_ROW_ID): CepEntity?
+    @Insert
+    suspend fun insertCep(cepEntity: CepEntity)
+
+    @Query("SELECT * FROM cep_table ORDER BY id DESC")
+    suspend fun getAllCeps(): List<CepEntity>
 }
